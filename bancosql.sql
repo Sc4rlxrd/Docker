9f401aa1572848ce8648ea854fcfde6f51b6a6aa
Sc4rlxrd <guilhermesantos2004gss@gmail.com>
2023-01-26 15:52:37 -0300

    Usando Docker para microsserviços em desafio de projeto dio

---
+++ bancosql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE dados (
+    AlunoID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50),
+    Sexo char(1) NOT NULL ,
+    Telefone varchar (20)
+);
